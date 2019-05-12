b996be1cbdbddb22817c99f0cbb71b5cfeb13857
Ziella Rose <ziellatheneko@gmail.com>
2019-05-13 01:12:37 +1000

    Finished up the new command class.

--- AOServer/source/Command.cs
+++ AOServer/source/Command.cs
@@ -9,17 +9,23 @@ namespace AOServer
     class Command
     {
 
-        public string command { get; set; }
-        public string[] Args { get; set; }
+        private string command { get; set; }
+        private string[] Args { get; set; }
 
         public Command(byte[] buffer)
         {
-
+            parse_string(Encoding.ASCII.GetString(buffer));
         }
 
         public Command(string _string)
         {
+            parse_string(_string);
+        }
 
+        public Command(string command, params string[] args)
+        {
+            this.command = command;
+            Args = args;
         }
 
         public byte[] Buffer()
@@ -30,6 +36,33 @@ namespace AOServer
         public string String()
         {
             return $"{command}#{string.Join("#", Args)}#%";
+        }
+
+        private void parse_string(string msg)
+        {
+            if (msg.StartsWith("#"))
+            {
+                msg = msg.Substring(1);
+                string[] spl = msg.Split('#');
+                command = FantaCrypt.fanta_decrypt(spl[0]);
+                Args = new string[] { spl[1] };
+            }
+            else
+            {
+
+                string[] spl = msg.Split('#');
+                command = spl[1];
+
+                string[] args = new string[spl.Count() - 1];
+
+                for (int i = 1; i < spl.Count(); i++)
+                {
+                    args[i - 1] = spl[i];
+                }
+
+                Args = args;
+
+            }
         }
 
 
